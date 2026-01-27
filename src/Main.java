261c35a38ccb07cacb99aa4c729881e837aa9467
Anna <foxy2ears@gmail.com>
2026-01-27 23:36:10 +0300

    Add simple bonus miles calculator

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticketPrice = 10000;
+        int rublesPerMile = 20;
+        int bonusMiles = ticketPrice / rublesPerMile;
+        System.out.println(bonusMiles);
+    }
+}
